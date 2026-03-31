696d87c90a297cc429c98c4a0e4f8287fbc38171
Alex Santos <alxmath@gmail.com>
2026-03-31 12:54:25 -0300

    test: adiciona testes de validação para GetTransactionsQuery cobrindo cenários inválidos e válidos

---
+++ FinTrack.Application.Tests/Transactions/Get/GetTransactionsValidatorTests.cs
@@ -0,0 +1,64 @@
+using FinTrack.Application.Transactions.Get;
+using FluentAssertions;
+
+namespace FinTrack.Application.Tests.Transactions.Get;
+
+public class GetTransactionsValidatorTests
+{
+    [Fact]
+    public void Validate_ShouldPass_WhenQueryIsValid()
+    {
+        // Arrange
+        var validator = new GetTransactionsValidator();
+
+        var query = new GetTransactionsQuery(1, 10);
+
+        // Act
+        var result = validator.Validate(query);
+
+        // Assert
+        result.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenPageNumberIsZero()
+    {
+        // Arrange
+        var validator = new GetTransactionsValidator();
+
+        var query = new GetTransactionsQuery(0, 10);
+
+        // Act
+        var result = validator.Validate(query);
+
+        // Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "PageNumber");
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenPageSizeIsZero()
+    {
+        var validator = new GetTransactionsValidator();
+
+        var query = new GetTransactionsQuery(1, 0);
+
+        var result = validator.Validate(query);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "PageSize");
+    }
+
+    [Fact]
+    public void Validate_ShouldFail_WhenPageSizeIsTooLarge()
+    {
+        var validator = new GetTransactionsValidator();
+
+        var query = new GetTransactionsQuery(1, 200);
+
+        var result = validator.Validate(query);
+
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().Contain(e => e.PropertyName == "PageSize");
+    }
+}
