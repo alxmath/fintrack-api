1f3fe2149e1d97996a80cab7b8d8eee3f0798b66
Alex Santos <alxmath@gmail.com>
2026-04-01 16:47:39 -0300

    feat: adicionar suporte a ordenação na query de transações

--- FinTrack.Application/Features/Transactions/Get/GetTransactionsQuery.cs
+++ FinTrack.Application/Features/Transactions/Get/GetTransactionsQuery.cs
@@ -9,4 +9,7 @@ public class GetTransactionsQuery
 
     public int Page { get; init; } = 1;
     public int PageSize { get; init; } = 10;
+     
+    public string? OrderBy { get; init; } = "date";
+    public bool Desc { get; init; } = true;
 }
