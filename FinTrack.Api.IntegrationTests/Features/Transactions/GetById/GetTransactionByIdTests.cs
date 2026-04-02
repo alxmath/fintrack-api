47fda6364334b8ad12905ff7a9e81f09b6b33c5b
Alex Santos <alxmath@gmail.com>
2026-04-02 20:15:01 -0300

    test: validar retorno 404 quando transação não existe

--- FinTrack.Api.IntegrationTests/Features/Transactions/GetById/GetTransactionByIdTests.cs
+++ FinTrack.Api.IntegrationTests/Features/Transactions/GetById/GetTransactionByIdTests.cs
@@ -3,6 +3,7 @@ using FinTrack.Application.Common.Results;
 using FinTrack.Application.Features.Transactions.Create;
 using FinTrack.Application.Features.Transactions.GetById;
 using FluentAssertions;
+using System.Net;
 using System.Net.Http.Json;
 
 namespace FinTrack.Api.IntegrationTests.Features.Transactions.GetById;
@@ -45,5 +46,18 @@ public class GetTransactionByIdTests : IntegrationTestBase
         result.IsSuccess.Should().BeTrue();
         result.Value.Should().NotBeNull();
         result.Value.Id.Should().Be(id);
+    }
+
+    [Fact]
+    public async Task GetById_ShouldReturn404_WhenTransactionDoesNotExist()
+    {
+        // Arrange
+        var id = Guid.NewGuid();
+
+        // Act
+        var response = await Client.GetAsync($"/api/v1/transactions/{id}");
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.NotFound);
     }
 }
