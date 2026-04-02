749fdf755a6735a6e9a315d46e31c1982c21a002
Alex Santos <alxmath@gmail.com>
2026-04-02 19:25:40 -0300

    feat: adicionar códigos de erro padronizados

---
+++ FinTrack.Application/Common/Errors/Errors.cs
@@ -0,0 +1,10 @@
+namespace FinTrack.Application.Common.Errors;
+
+public static class Errors
+{
+    public static class General
+    {
+        public const string NotFound = "NOT_FOUND";
+        public const string Validation = "VALIDATION_ERROR";
+    }
+}
