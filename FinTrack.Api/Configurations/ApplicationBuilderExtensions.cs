28ea3f84fa248fcf36b167e84d5c02b31115315d
Alex Santos <alxmath@gmail.com>
2026-04-05 15:54:13 -0300

    refactor: extrair pipeline HTTP para ApplicationBuilderExtensions

---
+++ FinTrack.Api/Configurations/ApplicationBuilderExtensions.cs
@@ -0,0 +1,25 @@
+using FinTrack.Api.Common.Middlewares;
+
+namespace FinTrack.Api.Configurations;
+
+public static class ApplicationBuilderExtensions
+{
+    public static WebApplication UseApiPipeline(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+
+        app.UseMiddleware<CorrelationIdMiddleware>();
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        return app;
+    }
+}
