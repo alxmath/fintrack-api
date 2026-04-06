816fc8a3854b7f6a2c0672727eae70b7bbad092d
Alex Santos <alxmath@gmail.com>
2026-04-06 17:12:21 -0300

    Revert "feat(application): adicionar interface base de pipeline (IPipelineBehavior)"
    This reverts commit 71da6aa0e0f5b98e3223a2604c6ddc4f8419f64a.

--- FinTrack.Application/Common/Pipeline/IPipelineBehavior.cs
+++
@@ -1,9 +0,0 @@
-namespace FinTrack.Application.Common.Pipeline;
-
-public interface IPipelineBehavior<TRequest, TResponse>
-{
-    Task<TResponse> Handle(
-        TRequest request,
-        CancellationToken cancellationToken,
-        Func<Task<TResponse>> next);
-}
