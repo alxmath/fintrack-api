71da6aa0e0f5b98e3223a2604c6ddc4f8419f64a
Alex Santos <alxmath@gmail.com>
2026-04-06 17:07:00 -0300

    feat(application): adicionar interface base de pipeline (IPipelineBehavior)

---
+++ FinTrack.Application/Common/Pipeline/IPipelineBehavior.cs
@@ -0,0 +1,9 @@
+namespace FinTrack.Application.Common.Pipeline;
+
+public interface IPipelineBehavior<TRequest, TResponse>
+{
+    Task<TResponse> Handle(
+        TRequest request,
+        CancellationToken cancellationToken,
+        Func<Task<TResponse>> next);
+}
