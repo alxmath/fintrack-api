01a3d38201a97fbfd9a09ebf9f769a0c3f31641d
Alex Santos <alxmath@gmail.com>
2026-04-15 21:29:00 -0300

    test(transactions): adicionar teste de categoria inexistente

--- FinTrack.Application.Tests/Features/Transactions/Create/CreateTransactionHandlerTests.cs
+++ FinTrack.Application.Tests/Features/Transactions/Create/CreateTransactionHandlerTests.cs
@@ -1,4 +1,5 @@
 using FinTrack.Application.Common.Interfaces;
+using FinTrack.Application.Common.Results;
 using FinTrack.Application.Features.Transactions.Create;
 using FinTrack.Domain.Entities;
 using FluentAssertions;
@@ -12,7 +13,8 @@ public class CreateTransactionHandlerTests
     public async Task Handle_ShouldCreateTransaction_WhenCommandIsValid()
     {
         // Arrange
-        var repositoryMock = new Mock<ITransactionRepository>();
+        var transactionRepositoryMock = new Mock<ITransactionRepository>();
+        var categoryRespositoryMock = new Mock<ICategoryRepository>();
         var userContextMock = new Mock<IUserContext>();
 
         var command = new CreateTransactionCommand(
@@ -22,14 +24,22 @@ public class CreateTransactionHandlerTests
             Guid.NewGuid()
         );
 
-        repositoryMock
+        var category = new Category("Test Category", Guid.NewGuid());
+
+        transactionRepositoryMock
             .Setup(r => r.AddAsync(It.IsAny<Transaction>(), It.IsAny<CancellationToken>()))
             .Returns(Task.CompletedTask);
+
+        categoryRespositoryMock
+            .Setup(c => c.GetByIdAsync(It.IsAny<Guid>(), It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(category);
 
         userContextMock.Setup(x => x.UserId).Returns(Guid.NewGuid());
 
         var handler = new CreateTransactionHandler(
-            repositoryMock.Object, userContextMock.Object);
+            transactionRepositoryMock.Object,
+            categoryRespositoryMock.Object,
+            userContextMock.Object);
 
         // Act
         var result = await handler.Handle(command, CancellationToken.None);
@@ -40,8 +50,52 @@ public class CreateTransactionHandlerTests
         result.Value.Should().NotBeNull();
         result.Value.Description.Should().Be("Salário");
 
-        repositoryMock.Verify(
+        transactionRepositoryMock.Verify(
             r => r.AddAsync(It.IsAny<Transaction>(), It.IsAny<CancellationToken>()),
             Times.Once);
+    }
+
+    [Fact]
+    public async Task Handle_Should_ReturnFailure_WhenCategoryDoesNotExist()
+    {
+        // Arrange
+        var transactionRepositoryMock = new Mock<ITransactionRepository>();
+        var categoryRespositoryMock = new Mock<ICategoryRepository>();
+        var userContextMock = new Mock<IUserContext>();
+
+        var command = new CreateTransactionCommand(
+            "Salário",
+            1000,
+            DateTime.UtcNow,
+            Guid.NewGuid()
+        );
+
+        userContextMock.Setup(x => x.UserId).Returns(Guid.NewGuid());
+
+        categoryRespositoryMock
+            .Setup(c => c.GetByIdAsync(
+                command.CategoryId,
+                userContextMock.Object.UserId,
+                It.IsAny<CancellationToken>()))
+            .ReturnsAsync(null as Category);
+
+        var handler = new CreateTransactionHandler(
+        transactionRepositoryMock.Object,
+        categoryRespositoryMock.Object,
+        userContextMock.Object);
+
+        // Act
+        var result = await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+        result.Value.Should().BeNull();
+        result.ErrorCode.Should().Be("NOT_FOUND");
+        result.Errors.Should().ContainKey("CategoryId");
+
+        transactionRepositoryMock.Verify(
+            r => r.AddAsync(It.IsAny<Transaction>(), It.IsAny<CancellationToken>()),
+            Times.Never);
     }
 }
